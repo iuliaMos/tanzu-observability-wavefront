9442dba784e1e59baf3e9b5b8ed083d58738d68a
Iulia <moscalenco.iulia@gmail.com>
2021-02-26 12:47:44 +0200

    web application that sends metrics to Tanzu Observability by Wavefront

---
+++ src/main/java/hello/DemoController.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "Greeting";
+    }
+}
